26592e8285881b896a3878b60795d18021129cb2
Amogh Nivaskar <amoghpnivas@gmail.com>
2026-06-28 21:16:21 +0530

    kt/06: per-thread trap handling via csrrw + sscratch for slot-based trapframe VA

--- kernel/trap.c
+++ kernel/trap.c
@@ -174,6 +174,8 @@ void prepare_return(void)
   // code to usertrap would be a disaster, turn off interrupts.
   intr_off();
 
+  w_sscratch(TRAPFRAME_BASE(mythread()->slot_index));
+
   // send syscalls, interrupts, and exceptions to uservec in trampoline.S
   uint64 trampoline_uservec = TRAMPOLINE + (uservec - trampoline);
   w_stvec(trampoline_uservec);
